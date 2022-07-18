9648edd248349c738a8863ae7628b85252675080
Gabriel Estevam <gabriel.estevam.narciso@gmail.com>
2022-07-18 19:30:13 -0300

    Tentiva de corrigir o build do storybook

--- quiz/.storybook/main.js
+++ quiz/.storybook/main.js
@@ -1,6 +1,8 @@
+const path = require("path");
 module.exports = {
   webpackFinal: async (config) => {
     const svelteLoader = config.module.rules.find( (r) => r.loader && r.loader.includes('svelte-loader'))
+    svelteLoader.path = [path.resolve(__dirname), "quiz/"]
     svelteLoader.options.preprocess = require('svelte-preprocess')()
     return config
   },
